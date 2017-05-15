839e450a747a0a5c934e0eab1f0015e9263c92bf
massaf <mahmoud.assaf@master.hes-so.ch>
2017-05-16 00:11:59 +0200

    When we click on image we load the scene (First Floor)

--- GOF/Assets/Scripts/LoadSceneOnClick.cs
+++ GOF/Assets/Scripts/LoadSceneOnClick.cs
@@ -7,8 +7,16 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneOnClick : MonoBehaviour {
 
+    public bool isImgClicked = false;
+
 	// Use this for initialization
 	public void LoadByIndex (int sceneIndex) {
         SceneManager.LoadScene (sceneIndex);
 	}
+
+    public void ImageClicked() {
+        //If building is clicked in Gallery we load the appropriate scene
+        isImgClicked = true;
+        LoadByIndex(1);
+    }
 }
